1bcbb69a68e358343236ad17a9b907dba073b6ff
Justin Patera <serialhex@gmail.com>
2013-05-07 17:46:51 -0400

    finishing #6... works well

--- src/cljs/modern_cljs/shopping.cljs
+++ src/cljs/modern_cljs/shopping.cljs
@@ -12,10 +12,11 @@
                                     (.toFixed 2)))
     false))
 
-(defn init []
+(defn ^:export init []
   (if (and js/document
            (.-getElementById js/document))
     (let [theForm (.getElementById js/document "shoppingForm")]
       (set! (.-onsubmit theForm) calculate))))
 
-(set! (.-onload js/window) init)
+;; t'was causing problems
+;; (set! (.-onload js/window) init)
